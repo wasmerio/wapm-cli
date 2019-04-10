afbb8e40d5f066a81104fed390d541cc21d11a37
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2019-04-10 16:44:16 -0700

    add decompressing and unpackaging to install command

--- src/commands/publish.rs
+++ src/commands/publish.rs
@@ -1,8 +1,10 @@
 use crate::manifest::Manifest;
 
-use crate::graphql::execute_query_modifier;
+use flate2::{write::GzEncoder, Compression};
 use graphql_client::*;
 use std::fs;
+use std::io::Write;
+use tar::Builder;
 
 #[derive(GraphQLQuery)]
 #[graphql(
@@ -13,24 +15,47 @@ use std::fs;
 struct PublishPackageMutation;
 
 pub fn publish() -> Result<(), failure::Error> {
+    let mut builder = Builder::new(Vec::new());
+
     let manifest = Manifest::find_in_current_directory()?;
-    let package = &manifest.package;
+    builder.append_path(manifest.manifest_path()?)?;
+    let module = manifest.module.as_ref().ok_or(PublishError::NoModule)?;
     let manifest_string = toml::to_string(&manifest)?;
-    let readme = package.readme.as_ref().and_then(|readme_path| {
+    let readme = module.readme.as_ref().and_then(|readme_path| {
+        if let Err(_) = builder.append_path(readme_path) {
+            // Maybe do something here
+        }
         fs::read_to_string(manifest.base_directory_path.join(readme_path)).ok()
     });
-    //    let module_path = manifest.module_path()?;
-    let _q = PublishPackageMutation::build_query(publish_package_mutation::Variables {
-        name: package.name.to_string(),
-        version: package.version.clone(),
-        description: package.description.clone(),
+    let module_path = manifest.module_path()?;
+    builder
+        .append_path(&module_path)
+        .map_err(|_| PublishError::NoModule)?;
+
+    let tar_archive_data = builder.into_inner().map_err(|_|
+                                                        // TODO:
+                                                        PublishError::NoModule)?;
+
+    //manifest.package.name
+    let archive_name = "test.tar.gz".to_string();
+    let archive_path = manifest.get_archive_path()?;
+    let mut compressed_archive = fs::File::create(archive_path.clone()).unwrap();
+    let mut gz_enc = GzEncoder::new(&mut compressed_archive, Compression::default());
+
+    gz_enc.write_all(&tar_archive_data).unwrap();
+    let _compressed_archive = gz_enc.finish().unwrap();
+
+    let q = PublishPackageMutation::build_query(publish_package_mutation::Variables {
+        name: module.name.to_string(),
+        version: module.version.clone(),
+        description: module.description.clone(),
         manifest: manifest_string,
         license: package.license.clone(),
         readme,
         file_name: Some("module".to_string()),
     });
-    //        let _response: publish_package_mutation::ResponseData =
-    //            execute_query_modifier(&q, |f| f.file("module", &module_path).unwrap())?;
+    let _response: publish_package_mutation::ResponseData =
+        execute_query_modifier(&q, |f| f.file(archive_name, archive_path).unwrap())?;
     Ok(())
 }
 
