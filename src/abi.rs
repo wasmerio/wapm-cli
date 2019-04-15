0e78da2b9a7aa15cbb5aa6b88f88c3891bcac9ea
Mark McCaskey <mark@wasmer.io>
2019-04-15 15:25:10 -0700

    add validation of ABI to validate command

--- src/abi.rs
+++ src/abi.rs
@@ -1,10 +1,31 @@
+use std::fmt;
+
+// TODO: update docstring
 /// The ABI is a hint to WebAssembly runtimes about what additional imports to insert. For the time
 /// being, this is a placeholder and does nothing. The default value is `None`.
-#[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
+#[derive(Clone, Copy, Debug, Deserialize, Serialize, PartialEq)]
 pub enum Abi {
+    #[serde(rename = "emscripten")]
     Emscripten,
+    // TODO: figure out if this makes sense
+    #[serde(rename = "")]
     None,
+    #[serde(rename = "wasi")]
     Wasi,
+}
+
+impl fmt::Display for Abi {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Abi::Emscripten => "emscripten",
+                Abi::Wasi => "wasi",
+                Abi::None => "undefined ABI",
+            }
+        )
+    }
 }
 
 impl Default for Abi {
--- src/commands/validate.rs
+++ src/commands/validate.rs
@@ -1,5 +1,6 @@
 use crate::manifest::Manifest;
 
+use crate::abi::Abi;
 use flate2::read::GzDecoder;
 use std::{fs, io::Read, path::PathBuf};
 use structopt::StructOpt;
@@ -86,7 +87,16 @@ pub fn validate_directory(pkg_path: PathBuf) -> Result<(), failure::Error> {
                     error: format!("{}", err),
                 }
             })?;
-            validate_wasm_and_report_errors(&wasm_buffer, source_path_string)?;
+            let detected_abi = validate_wasm_and_report_errors(&wasm_buffer, source_path_string)?;
+
+            if module.abi != Abi::None && module.abi != detected_abi {
+                return Err(ValidationError::MismatchedABI {
+                    module_name: module.name.clone(),
+                    found_abi: detected_abi,
+                    expected_abi: module.abi,
+                }
+                .into());
+            }
         }
     }
 
@@ -96,19 +106,51 @@ pub fn validate_directory(pkg_path: PathBuf) -> Result<(), failure::Error> {
 pub fn validate_wasm_and_report_errors(
     wasm: &[u8],
     file_name: String,
-) -> Result<(), failure::Error> {
+) -> Result<Abi, failure::Error> {
     use wasmparser::WasmDecoder;
     let mut parser = wasmparser::ValidatingParser::new(wasm, None);
+    let mut abi = Abi::None;
     loop {
         let state = parser.read();
         match *state {
-            wasmparser::ParserState::EndWasm => break Ok(()),
+            wasmparser::ParserState::EndWasm => return Ok(abi),
             wasmparser::ParserState::Error(e) => {
-                break Err(ValidationError::InvalidWasm {
+                return Err(ValidationError::InvalidWasm {
                     file: file_name,
                     error: format!("{}", e),
                 }
                 .into());
+            }
+            wasmparser::ParserState::ImportSectionEntry {
+                module: "wasi_unstable",
+                ..
+            } => {
+                if abi == Abi::None || abi == Abi::Wasi {
+                    abi = Abi::Wasi;
+                } else {
+                    return Err(ValidationError::MultipleABIs {
+                        file: file_name,
+                        first_abi: abi,
+                        second_abi: Abi::Wasi,
+                    }
+                    .into());
+                }
+            }
+            wasmparser::ParserState::ImportSectionEntry {
+                module: "env",
+                field: "_emscripten_memcpy_big",
+                ..
+            } => {
+                if abi == Abi::None || abi == Abi::Emscripten {
+                    abi = Abi::Emscripten;
+                } else {
+                    return Err(ValidationError::MultipleABIs {
+                        file: file_name,
+                        first_abi: abi,
+                        second_abi: Abi::Emscripten,
+                    }
+                    .into());
+                }
             }
             _ => {}
         }
@@ -126,6 +168,24 @@ pub enum ValidationError {
     MissingFile { file: String },
     #[fail(display = "Failed to read file {}; {}", file, error)]
     MiscCannotRead { file: String, error: String },
+    #[fail(
+        display = "Multiple ABIs detected in file {}; previously detected {} but found {}",
+        file, first_abi, second_abi
+    )]
+    MultipleABIs {
+        file: String,
+        first_abi: Abi,
+        second_abi: Abi,
+    },
+    #[fail(
+        display = "Detected ABI ({}) does not match ABI specified in wapm.toml ({}) for module \"{}\"",
+        found_abi, expected_abi, module_name
+    )]
+    MismatchedABI {
+        module_name: String,
+        found_abi: Abi,
+        expected_abi: Abi,
+    },
     #[fail(display = "Failed to unpack archive \"{}\"! {}", file, error)]
     CannotUnpackArchive { file: String, error: String },
 }
