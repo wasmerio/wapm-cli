743c1fdfe4ce9b8dd577bed183e92f5e1490b4b3
Mark McCaskey <mark@wasmer.io>
2019-04-16 16:11:58 -0700

    add init command to create wapm.toml

--- src/commands/completions.rs
+++ src/commands/completions.rs
@@ -1,7 +1,7 @@
 use structopt::{clap::Shell, StructOpt};
 
 #[derive(StructOpt, Debug)]
-pub struct CompletionOpts {
+pub struct CompletionOpt {
     /// The shell to generate the completions script for
     #[structopt(name = "SHELL", hidden = true, parse(try_from_str))]
     pub shell: Shell,
