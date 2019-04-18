be8c2149b8530bdee7ca35aac79fafa443518266
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2019-04-18 15:56:22 -0700

    wip

--- src/bonjour.rs
+++ src/bonjour.rs
@@ -1,10 +1,12 @@
 use crate::lock::lockfile::LockfileError;
-use crate::lock::{Lockfile, LOCKFILE_NAME, LockfileModule, LockfileCommand};
-use crate::manifest::{Manifest, ManifestError, MANIFEST_FILE_NAME, Package};
-use std::path::{Path, PathBuf};
+use crate::lock::{Lockfile, LockfileCommand, LockfileModule, LOCKFILE_NAME};
+use crate::manifest::{Manifest, MANIFEST_FILE_NAME};
+use std::cmp::Ordering;
+use std::collections::btree_map::BTreeMap;
+use std::collections::btree_set::BTreeSet;
+use std::path::Path;
 use std::{env, fs};
 use toml::Value;
-use std::collections::btree_map::BTreeMap;
 
 #[derive(Clone, Debug, Fail)]
 pub enum BonjourError {
@@ -12,15 +14,10 @@ pub enum BonjourError {
     E,
 }
 
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Clone, Debug, Eq, PartialOrd, PartialEq)]
 pub enum PackageId<'a> {
-    LocalPackage {
-        directory: &'a Path,
-    },
-    WapmRegistryPackage {
-        name: &'a str,
-        version: &'a str,
-    },
+    LocalPackage { directory: &'a Path },
+    WapmRegistryPackage { name: &'a str, version: &'a str },
     //    GitUrl {
     //        url: &'a str,
     //    },
@@ -30,19 +27,33 @@ pub enum PackageId<'a> {
     //    }
 }
 
-fn lockfile_to_packages<'a>(lockfile: Lockfile<'a>) -> Result<(Vec<PackageId<'a>>, BTreeMap<PackageId<'a>, PackageData<'a>>), BonjourError> {
-    let package_names = lockfile.modules.keys().cloned().collect::<Vec<&str>>();
-    let package_versions = package_names.iter().cloned().map(|n| {
-        lockfile.modules.get(n).unwrap().keys().map(|v| PackageId::WapmRegistryPackage { name: n.clone(), version: v, }).collect::<Vec<PackageId<'a>>>()
-    });
-    unimplemented!()
-}
-
-pub enum UnresolvedPackage {
-
-}
-
-pub enum InstalledPackage {
+impl<'a> Ord for PackageId<'a> {
+    fn cmp(&self, other: &PackageId<'a>) -> Ordering {
+        match (self, other) {
+            (
+                PackageId::WapmRegistryPackage { name, version },
+                PackageId::WapmRegistryPackage {
+                    name: other_name,
+                    version: other_version,
+                },
+            ) => {
+                let name_cmp = name.cmp(other_name);
+                let version_cmp = version.cmp(other_version);
+                match (name_cmp, version_cmp) {
+                    (Ordering::Equal, _) => version_cmp,
+                    _ => name_cmp,
+                }
+            }
+            (
+                PackageId::LocalPackage { directory },
+                PackageId::LocalPackage {
+                    directory: other_directory,
+                },
+            ) => directory.cmp(other_directory),
+            (PackageId::LocalPackage { .. }, _) => Ordering::Less,
+            (PackageId::WapmRegistryPackage { .. }, _) => Ordering::Greater,
+        }
+    }
 }
 
 pub enum PackageData<'a> {
@@ -50,16 +61,8 @@ pub enum PackageData<'a> {
         modules: Vec<LockfileModule<'a>>,
         commands: Vec<LockfileCommand<'a>>,
     },
-    ManifestDependencyPackage {
-
-    },
-    ManifestPackage {
-    },
-}
-
-struct LockfilePackage<'a> {
-    pub modules: Vec<LockfileModule<'a>>,
-    pub commands: Vec<LockfileCommand<'a>>,
+    ManifestDependencyPackage {},
+    ManifestPackage {},
 }
 
 #[derive(Clone, Debug)]
@@ -68,29 +71,89 @@ struct ManifestData {
 }
 
 impl<'a> ManifestData {
-    pub fn new(m: Manifest) -> Self {
-        ManifestData { value: Some(m) }
+    pub fn new_from_string(source: &String) -> Result<Self, BonjourError> {
+        toml::from_str::<Manifest>(source)
+            .map(|m| Self { value: Some(m) })
+            .map_err(|_| BonjourError::E)
     }
-    pub fn get_packages(&'a self) -> Option<Vec<PackageId<'a>>> { unimplemented!() }
+
+    pub fn get_packages(&'a self) -> Result<Option<BTreeSet<PackageId<'a>>>, BonjourError> {
+        match self.value {
+            Some(Manifest { ref package, dependencies: Some(ref dependencies), .. }) => {
+                // TODO, also pass along local package key
+                let package_ids = dependencies.iter().map(|(name, value)| {
+                    match value {
+                        Value::String(version) => Ok(PackageId::WapmRegistryPackage {
+                            name,
+                            version: &version,
+                        }),
+                        _ => Err(BonjourError::E),
+                    }
+                }).collect::<Result<BTreeSet<_>, BonjourError>>()
+                .map(|d| {
+                    if d.is_empty() {
+                        None
+                    }
+                    else {
+                        Some(d)
+                    }
+                });
+                package_ids
+            },
+            _ => {
+                Ok(None)
+            },
+        }
+    }
 }
+
 
 struct LockfileData<'a> {
     pub value: Option<Lockfile<'a>>,
 }
 
 impl<'a> LockfileData<'a> {
-    pub fn new(lockfile: Lockfile<'a>) -> Self {
-        LockfileData {
-            value: Some(lockfile),
-        }
+    pub fn new_from_string(source: &'a String) -> Result<Self, BonjourError> {
+        toml::from_str::<Lockfile>(source)
+            .map(|l| Self { value: Some(l) })
+            .map_err(|_| BonjourError::E)
     }
-    pub fn get_packages(&'a self) -> Option<(PackageId<'a>, LockfilePackage)> { unimplemented!() }
-}
 
-enum LockfileValue<'a> {
-    Lockfile(Lockfile<'a>),
-    NoLockfile,
-    LockfileError(LockfileError),
+    pub fn new_from_packages(packages: BTreeMap<PackageId<'a>, PackageData<'a>>) -> Self {
+        unimplemented!()
+    }
+
+    pub fn get_packages_and_package_data(
+        &'a self,
+    ) -> Option<(
+        BTreeSet<PackageId<'a>>,
+        BTreeMap<PackageId<'a>, PackageData<'a>>,
+    )> {
+        self.value.as_ref().map(|l| &l.modules).map(|modules| {
+            let package_ids = modules
+                .keys()
+                .cloned()
+                .into_iter()
+                .map(|n| {
+                    modules
+                        .get(n)
+                        .unwrap()
+                        .keys()
+                        .map(|v| PackageId::WapmRegistryPackage {
+                            name: n.clone(),
+                            version: v,
+                        })
+                        .collect::<Vec<PackageId<'a>>>()
+                })
+                .flatten()
+                .collect::<BTreeSet<PackageId<'a>>>();
+            (package_ids, BTreeMap::new())
+        })
+    }
+
+    pub fn save(self) -> Result<(), BonjourError> {
+        Ok(())
+    }
 }
 
 fn open_manifest_file(directory: &Path) -> Result<String, BonjourError> {
@@ -98,198 +161,85 @@ fn open_manifest_file(directory: &Path) -> Result<String, BonjourError> {
     fs::read_to_string(&manifest_path_buf).map_err(|_| BonjourError::E)
 }
 
-/// Construct a ManifestData from a string source
-fn manifest_source(source: &String) -> Result<ManifestData, BonjourError> {
-    toml::from_str::<Manifest>(source)
-        .map(ManifestData::new)
-        .map_err(|_| BonjourError::E)
-}
-
 fn open_lockfile(directory: &Path) -> Result<String, BonjourError> {
     let lockfile_path_buf = directory.join(LOCKFILE_NAME);
     fs::read_to_string(&lockfile_path_buf).map_err(|_| BonjourError::E)
 }
 
-/// Construct a ManifestData from a string source
-fn lockfile_source(source: &String) -> Result<LockfileData, BonjourError> {
-    toml::from_str::<Lockfile>(source)
-        .map(LockfileData::new)
-        .map_err(|_| BonjourError::E)
-}
-
-struct PackageManager;
-
-impl<'a> PackageManager {
-    fn new() -> Self {
-        PackageManager
-    }
-    fn add_packages_by_package_ids(&mut self, ids: Vec<PackageId>) -> Result<(), BonjourError> { unimplemented!() }
-}
-
-struct BonjourLogger;
-impl BonjourLogger {
-    fn log_err(error: BonjourError) {}
-    fn log_info(msg: String) {}
+fn calculate_differences<'a>(manifest_packages_set: Option<BTreeSet<PackageId<'a>>>, lockfile_packages_set: Option<BTreeSet<PackageId<'a>>>) -> (Vec<PackageId<'a>>, Vec<PackageId<'a>>, Vec<PackageId<'a>>) {
+    match (manifest_packages_set, lockfile_packages_set)
+        {
+            (Some(manifest_pkgs), Some(lockfile_pkgs)) => {
+                let added = manifest_pkgs
+                    .difference(&lockfile_pkgs)
+                    .cloned()
+                    .collect::<Vec<_>>();
+                let removed = lockfile_pkgs
+                    .difference(&manifest_pkgs)
+                    .cloned()
+                    .collect::<Vec<_>>();
+                let unchanged = manifest_pkgs
+                    .union(&lockfile_pkgs)
+                    .cloned()
+                    .collect::<Vec<_>>();
+                (added, removed, unchanged)
+            }
+            (Some(manifest_pkgs), None) => {
+                let added = manifest_pkgs.into_iter().collect::<Vec<_>>();
+                (added, vec![], vec![])
+            }
+            (None, Some(lockfile_pkgs)) => {
+                let unchanged = lockfile_pkgs.into_iter().collect::<Vec<_>>();
+                (vec![], vec![], unchanged)
+            }
+            (None, None) => (vec![], vec![], vec![]),
+        }
 }
 
 pub fn update() -> Result<(), BonjourError> {
     let directory = env::current_dir().map_err(|_| BonjourError::E)?;
+    // TODO add handler for case when manifest does not exist and a case when manifest is invalid
     let manifest_string = open_manifest_file(&directory);
-    let manifest_data = manifest_string.as_ref()
-        .map(manifest_source)
-        .map_err(BonjourError::clone)?.ok();
+    // TODO add handler for case when the lockfile is malformed, notify, but continue
     let lockfile_string = open_lockfile(&directory);
-    let lockfile_data = lockfile_string.as_ref()
-        .map(lockfile_source)
-        .map_err(BonjourError::clone)?.ok();
-
-    let mut pkg_mgr = PackageManager::new();
-
-    manifest_data.as_ref()
+    // deserialize and create manifest data
+    let manifest_data = manifest_string
+        .as_ref()
+        .map(ManifestData::new_from_string)
+        .map_err(BonjourError::clone)?;
+    // collect manifest dependency and local package keys, used later for diffing
+    let manifest_packages = manifest_data
+        .as_ref()
         .map(ManifestData::get_packages)
-        .unwrap_or_default()
-        .map(|manifest_package_ids| {
-            pkg_mgr.add_packages_by_package_ids(manifest_package_ids)
-        }).unwrap_or(Ok(()))?;
+        .unwrap_or(Ok(None))?;
+    // deserialize and create lockfile data
+    let lockfile_data = lockfile_string
+        .as_ref()
+        .map(LockfileData::new_from_string)
+        .map_err(BonjourError::clone)?
+        .ok();
+    // collect lockfile packages keys and package datas
+    let lockfile_packages = lockfile_data
+        .as_ref()
+        .map(LockfileData::get_packages_and_package_data)
+        .unwrap_or_default(); // flatten
 
-//    lockfile_data.as_ref()
-//        .map(LockfileData::get_packages)
-//        .unwrap_or_default()
-//        .map(|lockfile_package_ids| {
-//            pkg_mgr.add_packages_by_package_ids(manifest_package_ids)
-//        }).unwrap_or(Ok(()))?;
-    //    let lockfile_data = lockfile_source(shared_data.clone())?;
-    //    let project_data = create_project_data(manifest_data, lockfile_data)?;
-    //    let manifest_data = generate_manifest(project_data.clone())?;
-    //    let lockfile_data = generate_lockfile(project_data)?;
-    //    persist_manifest(manifest_data)?;
-    //    persist_lockfile(lockfile_data)?;
+    let (lockfile_packages, lockfile_packages_map) = match lockfile_packages {
+        Some((lockfile_packages, lockfile_packages_map)) => (Some(lockfile_packages), Some(lockfile_packages_map)),
+        None => (None, None),
+    };
+    // calculate diffs
+    let (added, removed, _unchanged) = calculate_differences(manifest_packages, lockfile_packages);
+
+    let mut packages_map = lockfile_packages_map.unwrap_or_default();
+
+    // prune dependencies that have been removed from dependencies list
+    removed.iter().map(|p| packages_map.remove(p)).for_each(drop);
+    // fetch and insert added packages
+    // TODO!
+
+    // serialize
+    let lockfile_data = LockfileData::new_from_packages(packages_map);
+    lockfile_data.save()?;
     Ok(())
 }
-
-
-//#[derive(Clone, Debug)]
-//enum PackageData {
-//    ManifestDependency {
-//        package_name: String,
-//        package_version: String,
-//    },
-//    ManifestPackage {
-//        package: Package,
-//    },
-////    LockfilePackage {
-////        package_name: String,
-////        package_version: String,
-////        modules: Vec<LockfileModule>,
-////        commands: Vec<LockfileCommand>,
-////    },
-//}
-//
-//#[derive(Clone, Debug)]
-//struct ProjectData {
-//    pub package_data: Vec<PackageData>,
-//}
-//
-//impl ProjectData {
-//    pub fn new_from_manifest_data(manifest_data: ManifestData) -> Result<Self, BonjourError> {
-//        match manifest_data.value {
-//            Some(Manifest {
-//                     package,
-//                     dependencies: Some(dependencies),
-//                     ..
-//                 }) => {
-//                let package_data = dependencies
-//                    .into_iter()
-//                    .map(|(name, value)| match value {
-//                        Value::String(version) => Ok(PackageData::ManifestDependency {
-//                            package_name: name,
-//                            package_version: version,
-//                        }),
-//                        _ => Err(BonjourError::E),
-//                    })
-//                    .chain(vec![Ok(PackageData::ManifestPackage {
-//                        package
-//                    })].into_iter())
-//                    .collect::<Result<Vec<PackageData>, BonjourError>>()?;
-//                Ok(ProjectData { package_data })
-//            }
-//            _ => Ok(ProjectData {
-//                package_data: vec![],
-//            }),
-//        }
-//    }
-//
-//    pub fn new_from_lockfile_data(lockfile_data: LockfileData) -> Result<Self, BonjourError> { Err(BonjourError::E)}
-//}
-
-//fn lockfile_source<'a, 's: 'a>(shared_data: SharedData<'s>) -> Result<LockfileData<'a>, BonjourError> {
-//    let manifest_path_buf = shared_data.directory.join(MANIFEST_FILE_NAME);
-//    match fs::read_to_string(&manifest_path_buf) {
-//        Ok(contents) => {
-//            let mut lockfile_data = LockfileData {
-//                value: None,
-//                source: Some(contents),
-//                shared_data,
-//            };
-//            if let Some(ref contents) = lockfile_data.source {
-//                match toml::from_str::<'a, Lockfile<'a>>(contents.as_str()) {
-//                    Ok(lockfile) => {
-//                        lockfile_data.value = Some(lockfile);
-//                    },
-//                    Err(toml_error) => {
-//                        return Err(BonjourError::E);
-//                    }
-//                }
-//            }
-//            Ok(lockfile_data)
-//        },
-//        Err(_io_error) => {
-//            Ok(LockfileData {
-//                value: None,
-//                source: None,
-//                shared_data,
-//            })
-//        }
-//    }
-//}
-
-//fn manifest_data_to_project_data(manifest_data: ManifestData) -> ProjectData {
-//
-//}
-
-//fn create_project_data<'a>(
-//    manifest_data: ManifestData,
-//    lockfile_data: LockfileData<'a>,
-//) -> Result<ProjectData, BonjourError> {
-//    Err(BonjourError::E)
-//}
-
-//fn generate_lockfile(project_data: ProjectData) -> Result<LockfileData, BonjourError> {
-//    unimplemented!()
-//    Ok(LockfileData::new(project_data.shared_data))
-//}
-
-//fn generate_manifest(project_data: ProjectData) -> Result<ManifestData, BonjourError> {
-//    unimplemented!()
-//    //    Ok(ManifestData::new(project_data.shared_data))
-//}
-
-//fn persist_manifest(manifest_data: ManifestData) -> Result<(), BonjourError> {
-//    match manifest_data.value {
-//        Some(manifest) => {
-//            manifest.save();
-//        }
-//        _ => {}
-//    }
-//    Ok(())
-//}
-
-//fn persist_lockfile(lockfile_data: LockfileData) -> Result<(), BonjourError> {
-//    match lockfile_data.value {
-//        Some(lockfile) => {
-//            lockfile.save(lockfile_data.shared_data.directory);
-//        }
-//        _ => {}
-//    }
-//    Ok(())
-//}
