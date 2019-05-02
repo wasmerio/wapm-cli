111edf8bf0289f5dcbc74c411073b05be755654c
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2019-05-02 12:00:11 -0700

    display an error if user tries to uninstall with version number

--- src/commands/uninstall.rs
+++ src/commands/uninstall.rs
@@ -3,6 +3,12 @@ use crate::dataflow;
 use std::env;
 use structopt::StructOpt;
 
+#[derive(Clone, Debug, Fail)]
+pub enum Error {
+    #[fail(display = "Packages may be uninstalled with the package name only.")]
+    NoAtSignAllowed,
+}
+
 #[derive(StructOpt, Debug)]
 pub struct UninstallOpt {
     pub package: String,
@@ -17,6 +23,11 @@ pub fn uninstall(options: UninstallOpt) -> Result<(), failure::Error> {
         false => env::current_dir()?,
     };
     let uninstalled_package_names = vec![options.package.as_str()];
+
+    // do not allow the "@" symbol to prevent mis-use of this command
+    if options.package.contains('@') {
+        return Err(Error::NoAtSignAllowed.into());
+    }
 
     // returned bool indicates if there was any to the lockfile. If this pacakge is uninstalled,
     // there will be a diff created, which causes update to return true. Because no other change
