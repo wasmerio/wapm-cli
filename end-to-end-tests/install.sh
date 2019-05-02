b804d028346111f6055e9dc90a3c030362cba0d7
Mark McCaskey <mark@wasmer.io>
2019-05-02 11:26:47 -0700

    update test

--- end-to-end-tests/install.sh
+++ end-to-end-tests/install.sh
@@ -3,7 +3,14 @@
 alias wapm=target/debug/wapm
 wapm config set registry.url "https://registry.wapm.dev"
 wapm install cowsay@0.1.2
+wapm install cowsay@0.1.2
 wapm run cowsay "hello, world"
 wapm list
 wapm uninstall cowsay
-wapm list
+wapm install cowsay@0.1.2
+wapm uninstall cowsay
+wapm uninstall cowsay
+wapm install -g mark/rust-example@0.1.9
+wapm run hq9+ -e "H"
+wapm uninstall -g mark/rust-example
+wapm list -a
