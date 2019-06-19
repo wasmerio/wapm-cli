438b78c734a05217c47c7d5325364f8ca2b2e475
Mark McCaskey <MarkMcCaskey@users.noreply.github.com>
2019-06-19 10:49:37 -0700

    fix package signing bug; improve ergonomics of installing duplicates (#121)
    * fix package signing bug; improve ergonomics of installing duplicates
    
    * add error on duplicate install to regression test
    
    * explicitly clean up output files to prevent failures from succeeding
    
    * try returning result of diff, non-zero return should be failing
    
    * redirect stderr to the file, too
    
    * try to make this test more consistent and reproducible
    
    * oops missed this
    
    * make error stable
    
    * clean up results

--- end-to-end-tests/verification.sh
+++ end-to-end-tests/verification.sh
@@ -2,10 +2,12 @@
 
 #alias wapm=target/debug/wapm
 wapm config set registry.url "https://registry.wapm.dev"
-yes no | wapm install mark2/dog2@0.0.0
+# redirect stderr to /dev/null so we can capture important stderr
+yes no 2> /dev/null | wapm install mark2/dog2@0.0.0
+# wc because the date changes
 wapm keys list -a
-yes | wapm install mark2/dog@0.0.4
-wapm keys list -a
+yes 2> /dev/null | wapm install mark2/dog@0.0.4
+wapm keys list -a | wc -l
 wapm uninstall mark2/dog
 wapm install mark2/dog@0.0.4
 wapm install mark2/dog2@0.0.0
