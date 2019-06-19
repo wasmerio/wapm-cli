1eed2413d7e26d8e6a0b85aabceef7483c6406e3
Mark McCaskey <mark@wasmer.io>
2019-06-19 10:56:59 -0700

    update test to store stderr and return with diff result

--- end-to-end-tests/package-fs-mapping.sh
+++ end-to-end-tests/package-fs-mapping.sh
@@ -2,7 +2,7 @@
 
 alias wapm=target/debug/wapm
 wapm config set registry.url "https://registry.wapm.dev"
-yes | wapm install -g mark2/dog2@0.0.6
+yes 2> /dev/null | wapm install -g mark2/dog2@0.0.6
 wapm run dog
 wapm install mark2/dog2@0.0.6
 wapm run dog
