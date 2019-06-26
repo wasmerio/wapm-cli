4893ca54a10d24b3ad16d934ed9d90f526ae2afe
Mark McCaskey <mark@wasmer.io>
2019-06-26 15:19:46 -0700

    fix up test

--- end-to-end-tests/manifest-validation.sh
+++ end-to-end-tests/manifest-validation.sh
@@ -7,6 +7,8 @@ wapm publish --dry-run
 # get a wasm module so we forget the abi field
 yes 2> /dev/null | wapm install mark2/dog2@0.0.13
 cp wapm_packages/mark2/dog2@0.0.13/dog.wasm .
-echo '[package]\nname="test"\nversion="0.0.0"\ndescription="this is a test"\n[[module]]\nname="test-module"\nsource="dog.wasm"\n[[command]]\nname="test"\nmodule="test"\n[fs]\n"wapm_file"="src/bin"' > wapm.toml
+echo '[package]\nname="test"\nversion="0.0.0"\ndescription="this is a test"\n[[module]]\nname="test-module"\nsource="dog.wasm"\n[[command]]\nname="test"\nmodule="test-module"\n[fs]\n"wapm_file"="src/bin"' > wapm.toml
+wapm publish --dry-run
+echo '[package]\nname="test"\nversion="0.0.0"\ndescription="this is a test"\n[[module]]\nname="test-module"\nsource="dog.wasm"\nabi="wasi"\n[[command]]\nname="test"\nmodule="test-module"\n[fs]\n"wapm_file"="src/bin"' > wapm.toml
 wapm publish --dry-run
 rm dog.wasm
