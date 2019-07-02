6255c8f134c4b5a1406fd5c6f3d384d507da3458
Mark McCaskey <mark@wasmer.io>
2019-07-02 12:03:39 -0700

    hmm, context switching, no idea why this test is failing

--- end-to-end-tests/package-fs-mapping.sh
+++ end-to-end-tests/package-fs-mapping.sh
@@ -9,6 +9,6 @@ wapm install mark2/dog2@0.0.13
 wapm run dog -- data
 wapm uninstall mark2/dog2
 cp wapm_packages/mark2/dog2@0.0.13/dog.wasm .
-echo '[package]\nname="test"\nversion="0.0.0"\ndescription="this is a test"\n[[module]]\nname="test"\nsource="dog.wasm"\n[[command]]\nname="test"\nmodule="test"\n[fs]\n"wapm_file"="src/bin"' > wapm.toml
+echo '[package]\nname="test"\nversion="0.0.0"\ndescription="this is a test"\n[[module]]\nname="test-module"\nsource="dog.wasm"\n[[command]]\nname="test"\nmodule="test-module"\n[fs]\n"wapm_file"="src/bin"' > wapm.toml
 wapm run test -- wapm_file
 rm dog.wasm
