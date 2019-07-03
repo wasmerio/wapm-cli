97ad4a52fab60a54d67bc190ad1ddac35523bf86
Mark McCaskey <MarkMcCaskey@users.noreply.github.com>
2019-07-03 14:18:53 -0700

    fix key signing bug with globally namespaced packages (#133)
    * fix key signing bug with globally namespaced packages
    
    * update verification test to test new behavior
    
    * fix verification test, add debug statements to find _command_result
    
    * add step to print wapm.log if test failed
    
    * add more debug calls :shrug:

--- src/dataflow/find_command_result.rs
+++ src/dataflow/find_command_result.rs
@@ -92,6 +92,7 @@ impl FindCommandResult {
         match lockfile.get_command(command_name.as_ref()) {
             Err(e) => FindCommandResult::Error(e),
             Ok(lockfile_command) => {
+                debug!("Command found in lockfile: {:?}", &lockfile_command);
                 if lockfile_command.package_name == manifest.package.name {
                     // this is a local module command
                     let found_module = manifest.module.as_ref().and_then(|modules| {
@@ -113,6 +114,10 @@ impl FindCommandResult {
                         ),
                     }
                 } else {
+                    debug!(
+                        "Command's package name({}) and manifest's package name({}) are different",
+                        lockfile_command.package_name, manifest.package.name
+                    );
                     match lockfile.get_module(
                         &lockfile_command.package_name,
                         &lockfile_command.package_version,
@@ -173,6 +178,7 @@ impl FindCommandResult {
             (_, LockfileResult::LockfileError(e)) => return FindCommandResult::Error(e.into()),
             (ManifestResult::NoManifest, LockfileResult::NoLockfile) => {} // continue
             (ManifestResult::NoManifest, LockfileResult::Lockfile(l)) => {
+                debug!("Looking for local command in the lockfile");
                 return Self::find_command_in_lockfile(command_name, l);
             }
             // the edge case of a manifest, but no lockfile would an invalid state. This function
@@ -183,6 +189,7 @@ impl FindCommandResult {
                 panic!("Manifest exists, but lockfile not found!")
             }
             (ManifestResult::Manifest(m), LockfileResult::Lockfile(l)) => {
+                debug!("Looking for local command in the manifest and lockfile");
                 return Self::find_command_in_manifest_and_lockfile(command_name, m, l);
             }
         };
