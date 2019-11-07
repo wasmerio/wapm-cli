7a34a47d50f7c2c93acad7688041317d888ffbe0
Mark McCaskey <mark@wasmer.io>
2019-11-06 16:12:03 -0800

    fix up CI for real this time

--- end-to-end-tests/init-and-add.sh
+++ end-to-end-tests/init-and-add.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 alias wapm=target/debug/wapm
+mkdir test-package
+cd test-package
 wapm config set registry.url "https://registry.wapm.dev"
 wapm init -y
 wapm add this-package-does-not-exist
 wapm add mark2/python@0.0.4 mark2/dog2
 cat wapm.toml
+cd ..
+rm -rf test-package
