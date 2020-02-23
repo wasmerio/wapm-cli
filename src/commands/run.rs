eaafbb8a11525ba2cb9e82eb9f5a6dab4137037a
Syrus <me@syrusakbary.com>
2020-02-23 12:39:33 -0800

    Improved linting

--- src/commands/run.rs
+++ src/commands/run.rs
@@ -1,10 +1,10 @@
 use crate::config::Config;
-use crate::util::get_runtime;
 use crate::data::lock::is_lockfile_out_of_date;
 use crate::dataflow;
 use crate::dataflow::find_command_result;
 use crate::dataflow::find_command_result::get_command_from_anywhere;
 use crate::dataflow::manifest_packages::ManifestResult;
+use crate::util::get_runtime;
 use std::env;
 use std::ffi::OsString;
 use std::path::{Path, PathBuf};
@@ -135,16 +135,17 @@ pub fn run(run_options: RunOpt) -> Result<(), failure::Error> {
     )?;
     debug!("Running command with args: {:?}", command_vec);
     let runtime = get_runtime();
-    let mut child = Command::new(&runtime)
-        .args(&command_vec)
-        .spawn()
-        .map_err(|e| -> failure::Error {
-            RunError::ProcessFailed {
-                runtime: runtime,
-                error: format!("{:?}", e),
-            }
-            .into()
-        })?;
+    let mut child =
+        Command::new(&runtime)
+            .args(&command_vec)
+            .spawn()
+            .map_err(|e| -> failure::Error {
+                RunError::ProcessFailed {
+                    runtime: runtime,
+                    error: format!("{:?}", e),
+                }
+                .into()
+            })?;
 
     child.wait()?;
     Ok(())
