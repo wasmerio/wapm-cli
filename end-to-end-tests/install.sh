7517d31dce08d7ea44d2007541a77ff98a900e12
Manos Pitsidianakis <manos@wasmer.io>
2022-04-07 11:44:22 +0300

    Update expected regression test output

--- end-to-end-tests/install.sh
+++ end-to-end-tests/install.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 
 wapm config set registry.url "https://registry.wapm.dev"
-wapm install cowsay@0.1.2
-wapm install cowsay@0.1.2
+wapm install namespace-example/cowsay@0.1.2
+wapm install namespace-example/cowsay@0.1.2
 wapm run cowsay "hello, world"
 wapm list
-wapm uninstall cowsay
-wapm install cowsay@0.1.2
-wapm uninstall cowsay
-wapm uninstall cowsay
+wapm uninstall namespace-example/cowsay
+wapm install namespace-example/cowsay@0.1.2
+wapm uninstall namespace-example/cowsay
+wapm uninstall namespace-example/cowsay
 wapm install -g mark/rust-example@0.1.11
 wapm run hq9+ -e "H"
 wapm uninstall -g mark/rust-example
-wapm list -a
 wapm install -g mark/wapm-override-test@0.1.0
+wapm list -a
 wapm run wapm-override-test
 wapm install mark/wapm-override-test@0.2.0
 wapm run wapm-override-test
 wapm uninstall mark/wapm-override-test
 wapm run wapm-override-test
-wapm install cowsay@0.1.1 cowsay@0.1.2
+wapm uninstall -g mark/wapm-override-test
+wapm install namespace-example/cowsay@0.1.1 namespace-example/cowsay@0.1.2
