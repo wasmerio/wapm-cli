8bf7f3d611575c073605f8a2d9d1ff14283b28a7
Felix Schütt <felix@wasmer.io>
2022-08-11 17:49:25 +0200

    Remove entire wapm_resolve_url crate on WASI

--- wapm-toml/src/lib.rs
+++ wapm-toml/src/lib.rs
@@ -70,21 +70,23 @@ pub fn get_dependencies(wapm: &str) -> Vec<(String, String)>{
     .iter().map(|(k, v)| (k.clone(), v.clone()))
     .collect::<Vec<_>>();
 
-    let current_registry = wapm_resolve_url::get_current_wapm_registry();
+    #[cfg(not(target_os = "wasi"))] {
+        let current_registry = wapm_resolve_url::get_current_wapm_registry();
 
-    for (k, _) in dependencies.iter_mut() {
-        if k.split("/").count() == 1 {
-            // Somebody only specified the package / command as the dependency instead
-            // of using the owner/package format
-            if let Some(r) = current_registry.as_ref() {
-                let package_info = wapm_resolve_url::get_tar_gz_url_of_package(r, k, None);
-                if let Some(pi) = package_info {
-                    *k = pi.resolved_name.clone();
+        for (k, _) in dependencies.iter_mut() {
+            if k.split("/").count() == 1 {
+                // Somebody only specified the package / command as the dependency instead
+                // of using the owner/package format
+                if let Some(r) = current_registry.as_ref() {
+                    let package_info = wapm_resolve_url::get_tar_gz_url_of_package(r, k, None);
+                    if let Some(pi) = package_info {
+                        *k = pi.resolved_name.clone();
+                    }
                 }
             }
-        }
+        }    
     }
-
+    
     dependencies
 }
 
