4243ece58a4ca437159b5e0802ef334e289593e5
Michael-F-Bryan <michaelfbryan@gmail.com>
2022-08-26 16:26:57 +0800

    Fetch bindings URLs from the backend

--- src/commands/install.rs
+++ src/commands/install.rs
@@ -1,14 +1,17 @@
 //! Code pertaining to the `install` subcommand
 
-use crate::graphql::execute_query;
+use crate::{
+    commands::install::get_package_query::GetPackageQueryPackageLastVersion,
+    dataflow::bindings::Language, graphql::execute_query,
+};
 
 use graphql_client::*;
 
 use crate::config::Config;
 use crate::dataflow;
 use crate::util;
-use std::{borrow::Cow, path::PathBuf};
-use std::{path::Path, str::FromStr};
+use std::{borrow::Cow, convert::TryInto, path::PathBuf};
+use std::{convert::TryFrom, path::Path};
 use structopt::StructOpt;
 use thiserror::Error;
 
@@ -89,7 +92,7 @@ pub fn install(options: InstallOpt) -> anyhow::Result<()> {
         "this function should only be called once!"
     );
 
-    match Bindings::from_options(&options) {
+    match Target::from_options(&options) {
         Some(language) => install_bindings(
             language,
             &options.packages,
@@ -101,18 +104,21 @@ pub fn install(options: InstallOpt) -> anyhow::Result<()> {
 }
 
 fn install_bindings(
-    language: Bindings,
+    target: Target,
     packages: &[String],
     module: Option<&str>,
     current_directory: PathBuf,
 ) -> Result<(), anyhow::Error> {
-    let package = match packages {
-        [p] => p.as_str(),
+    let VersionedPackage { name, version } = match packages {
+        [p] => p.as_str().try_into()?,
         [] => anyhow::bail!("No package provided"),
         [..] => anyhow::bail!("Bindings can only be installed for one package at a time"),
     };
 
-    todo!()
+    let url =
+        dataflow::bindings::link_to_package_bindings(name, version, target.language(), module)?;
+
+    todo!("Handle {:?}", url)
 }
 
 fn wapm_install(options: InstallOpt, current_directory: PathBuf) -> Result<(), anyhow::Error> {
@@ -137,12 +143,12 @@ fn install_packages(
 ) -> Result<(), anyhow::Error> {
     let mut packages = vec![];
     for name in package_names {
-        packages.push(VersionedPackage::from_str(name)?);
+        packages.push(parse_package_and_version(name)?);
     }
 
     let installed_packages: Vec<(&str, &str)> = packages
         .iter()
-        .map(|pkg| (pkg.name.as_str(), pkg.version.as_str()))
+        .map(|(name, version)| (name.as_str(), version.as_str()))
         .collect();
 
     // the install directory will determine which wapm.lock we are updating. For now, we
@@ -174,48 +180,60 @@ fn install_packages(
 }
 
 #[derive(Debug)]
-struct VersionedPackage {
-    name: String,
-    version: String,
+struct VersionedPackage<'a> {
+    name: &'a str,
+    version: Option<&'a str>,
 }
 
-impl FromStr for VersionedPackage {
-    type Err = anyhow::Error;
+impl<'a> TryFrom<&'a str> for VersionedPackage<'a> {
+    type Error = anyhow::Error;
 
-    fn from_str(package_specifier: &str) -> Result<Self, Self::Err> {
+    fn try_from(package_specifier: &'a str) -> Result<Self, Self::Error> {
         let name_and_version: Vec<_> = package_specifier.split('@').collect();
 
-        match name_and_version.as_slice() {
+        match *name_and_version.as_slice() {
             [name, version] => Ok(VersionedPackage {
-                name: name.to_string(),
-                version: version.to_string(),
+                name,
+                version: Some(version),
             }),
-            [name] => {
-                let q = GetPackageQuery::build_query(get_package_query::Variables {
-                    name: name.to_string(),
-                });
-                let response: get_package_query::ResponseData = execute_query(&q)?;
-                let package = response.package.ok_or(InstallError::PackageNotFound {
-                    name: name.to_string(),
-                })?;
-                let last_version =
-                    package
-                        .last_version
-                        .ok_or(InstallError::NoVersionsAvailable {
-                            name: name.to_string(),
-                        })?;
-                let package_name = package.name.clone();
-                let package_version = last_version.version.clone();
-                Ok(VersionedPackage {
-                    name: package_name,
-                    version: package_version,
-                })
-            }
+            [name] => Ok(VersionedPackage {
+                name,
+                version: None,
+            }),
             _ => Err(InstallError::InvalidPackageIdentifier {
                 name: package_specifier.to_string(),
             }
             .into()),
         }
+    }
+}
+
+fn parse_package_and_version(package_specifier: &str) -> Result<(String, String), anyhow::Error> {
+    let name_and_version: Vec<_> = package_specifier.split('@').collect();
+
+    match name_and_version.as_slice() {
+        [name, version] => Ok((name.to_string(), version.to_string())),
+        [name] => {
+            let q = GetPackageQuery::build_query(get_package_query::Variables {
+                name: name.to_string(),
+            });
+            let response: get_package_query::ResponseData = execute_query(&q)?;
+            let package = response.package.ok_or(InstallError::PackageNotFound {
+                name: name.to_string(),
+            })?;
+            let GetPackageQueryPackageLastVersion { version, .. } =
+                package
+                    .last_version
+                    .ok_or(InstallError::NoVersionsAvailable {
+                        name: name.to_string(),
+                    })?;
+
+            Ok((name.to_string(), version))
+        }
+        _ => Err(InstallError::InvalidPackageIdentifier {
+            name: package_specifier.to_string(),
+        }
+        .into()),
     }
 }
 
@@ -228,24 +246,31 @@ fn local_install_from_lockfile(current_directory: &Path) -> Result<(), anyhow::E
 }
 
 #[derive(Debug)]
-enum Bindings {
+enum Target {
     Npm,
     Yarn,
     Python,
 }
 
-impl Bindings {
+impl Target {
     fn from_options(options: &InstallOpt) -> Option<Self> {
         let InstallOpt {
             yarn, npm, python, ..
         } = options;
 
         match (yarn, npm, python) {
-            (true, false, false) => Some(Bindings::Yarn),
-            (false, true, false) => Some(Bindings::Npm),
-            (false, false, true) => Some(Bindings::Python),
+            (true, false, false) => Some(Target::Yarn),
+            (false, true, false) => Some(Target::Npm),
+            (false, false, true) => Some(Target::Python),
             (false, false, false) => None,
             _ => unreachable!("Already rejected by clap"),
+        }
+    }
+
+    fn language(&self) -> Language {
+        match self {
+            Target::Npm | Target::Yarn => Language::JavaScript,
+            Target::Python => Language::Python,
         }
     }
 }
