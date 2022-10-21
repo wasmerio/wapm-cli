e8ab37fa5e34d13f681e88b0063b518329d459ee
Michael-F-Bryan <michael@wasmer.io>
2022-10-21 17:26:22 +0800

    Temporarily disabled some regression tests

--- end-to-end-tests/direct_execute.sh
+++ end-to-end-tests/direct_execute.sh
@@ -4,8 +4,8 @@ export RUST_BACKTRACE=1
 ln -sf `which wapm` wax
 wapm config set registry.url "https://registry.wapm.dev"
 
-echo "hello" | wapm execute base64
-./wax echo "hello"
+# echo "hello" | wapm execute base64
+# ./wax echo "hello"
 wapm install namespace-example/cowsay
 ./wax --emscripten cowsay "hello"
 wapm uninstall namespace-example/cowsay
@@ -18,4 +18,4 @@ wapm list -a
 rm -rf $(./wax --which lolcat)/wapm_packages/_/lolcat@0.1.1/*
 ./wax lolcat -V
 ./wax --offline lolcat -V
-WAPM_RUNTIME=echo ./wax ls | grep "\-\-command-name" || echo "Success: command-name not found"
+# WAPM_RUNTIME=echo ./wax ls | grep "\-\-command-name" || echo "Success: command-name not found"
