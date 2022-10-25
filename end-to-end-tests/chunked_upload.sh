a74b3f536f257fe262790fae3741c35cee698aa8
Felix Schütt <felix@wasmer.io>
2022-10-25 15:48:29 +0200

    Add regression test for uploading packages to wapm.dev

---
+++ end-to-end-tests/chunked_upload.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+export RUST_BACKTRACE=1
+ln -sf `which wapm` wax
+wapm config set registry.url "https://registry.wapm.dev/graphql"
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PWD=$(pwd -P)
+RANDOMVERSION1=$RANDOM
+RANDOMVERSION2=$RANDOM
+RANDOMVERSION3=$RANDOM
+
+mkdir -p /tmp/largewasmfile
+cp -rf $SCRIPT_DIR/../assets/largewasmfile.wasm /tmp/largewasmfile/largewasmfile.wasm
+cp -rf $SCRIPT_DIR/../assets/largewasmfile.wapm.toml /tmp/largewasmfile/wapm.toml
+cp -rf $SCRIPT_DIR/chunked_upload.txt /tmp/chunked_upload_reference.txt
+
+if [ "$(uname)" == "Darwin" ]; then
+    sed -i '' "s/RANDOMVERSION3/$RANDOMVERSION3/g" /tmp/largewasmfile/wapm.toml
+    sed -i '' "s/RANDOMVERSION2/$RANDOMVERSION2/g" /tmp/largewasmfile/wapm.toml
+    sed -i '' "s/RANDOMVERSION1/$RANDOMVERSION1/g" /tmp/largewasmfile/wapm.toml
+
+    sed -i '' "s/RANDOMVERSION3/$RANDOMVERSION3/g" /tmp/chunked_upload_reference.txt
+    sed -i '' "s/RANDOMVERSION2/$RANDOMVERSION2/g" /tmp/chunked_upload_reference.txt
+    sed -i '' "s/RANDOMVERSION1/$RANDOMVERSION1/g" /tmp/chunked_upload_reference.txt
+else
+    sed -i "s/RANDOMVERSION3/$RANDOMVERSION3/g" /tmp/largewasmfile/wapm.toml
+    sed -i "s/RANDOMVERSION2/$RANDOMVERSION2/g" /tmp/largewasmfile/wapm.toml
+    sed -i "s/RANDOMVERSION1/$RANDOMVERSION1/g" /tmp/largewasmfile/wapm.toml
+
+    sed -i "s/RANDOMVERSION3/$RANDOMVERSION3/g" /tmp/chunked_upload_reference.txt
+    sed -i "s/RANDOMVERSION2/$RANDOMVERSION2/g" /tmp/chunked_upload_reference.txt
+    sed -i "s/RANDOMVERSION1/$RANDOMVERSION1/g" /tmp/chunked_upload_reference.txt
+fi
+
+cd /tmp/largewasmfile
+# wapm login --token ${{ secrets.WAPM_DEV_TOKEN }}
+$SCRIPT_DIR/../target/release/wapm publish
+cd $PWD
---
+++ end-to-end-tests/ci/chunked-upload.sh
@@ -0,0 +1,26 @@
+export PATH=$PATH:$HOME/.cargo/bin
+export PATH=$PATH:$HOME/.wasmer/bin
+export WAPM_DISABLE_COLOR=true
+rm -f $WASMER_DIR/.wax_index.toml
+# TODO force clear cache
+rm -f wapm.toml
+rm -f wapm.lock
+chmod +x end-to-end-tests/chunked_upload.sh
+echo "RUNNING SCRIPT..."
+./end-to-end-tests/chunked_upload.sh &> /tmp/chunked_upload.txt
+echo "GENERATED OUTPUT:"
+cat /tmp/chunked_upload.txt
+echo "COMPARING..."
+diff -Bba /tmp/chunked_upload_reference.txt /tmp/chunked_upload.txt
+export OUT=$?
+if ( [ -d globals ] || [ -f wapm.log ] ) then
+   { echo "globals or wapm.log found; these files should not be in the working directory"; exit 1; }
+   else { true; }
+fi
+
+rm -f wapm.lock
+rm -f wapm.toml
+rm -rf wapm_packages
+rm -f /tmp/chunked_upload.txt
+if ( [ $OUT -ne 0 ] ) then { cat $HOME/.wasmer/wapm.log; } fi
+exit $OUT
